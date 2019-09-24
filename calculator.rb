979adf29703af6e176a23f8cd10dc79ad82fd8a9
Mason Waller <masonwaller805@gmail.com>
2019-09-24 21:08:25 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,7 +1,7 @@
 # Add your variables here
 first_number = 10
 second_number= 6
-sum = 10 + 6
-difference =10 - 6
-product = 10*6
+sum = 10 + second_number
+difference =10 - second_number
+product = 10*second_number
 quotient= first_number/second_number
